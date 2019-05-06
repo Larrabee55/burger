67e2c2e1149425e3a5052aac6bab462e12c5f1a6
Cameron Larrabee <cameronlarrabee@yahoo.com>
2019-05-05 19:35:28 -0600

    pulls the functions/method from the ORM file for the database functions and exports the file

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,22 @@
-require("../config/orm")(mySQLCommands);
+var orm = require("../config/orm.js");
 
-module.exports = function (ORMFunctions) {
+var burger = {
   // functions that call ORM functions using specific input for the ORM
+  all: function (cb) {
+    orm.all("burgers", function (res) {
+      cb(res);
+    });
+  },
+  create: function (cols, vals, cb) {
+    orm.create("burgers", cols, vals, function (res) {
+      cb(res);
+    });
+  },
+  update: function (objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function (res) {
+      cb(res)
+    });
+  }
 }
+// export the database functions for the controller (burgers_controllers.js)
+module.exports = burger;
